665de45d24ce09d8a3fe555161bff85d58e60ac2
citrus-candy <kyuuri11013@gmail.com>
2021-11-29 04:47:32 +0900

    [add] ヘッダーのリンク部分を作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,30 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const HeaderStyle = styled.div`
+	font-size: 48px;
+	display: flex;
+	justify-content: flex-end;
+`
+
+const LinkStyle = styled.div`
+	text-align: center;
+	padding: 0 40px;
+	font-family: 'Fredericka the Great', cursive;
+`
 
 export default function Header() {
 	return (
-		<div className="header">
-			<Link to="/">Top</Link>
-			<Link to="/works">Works</Link>
-			<Link to="/about">About</Link>
-		</div>
+		<HeaderStyle>
+			<LinkStyle>
+				<Link to="/">Top</Link>
+			</LinkStyle>
+			<LinkStyle>
+				<Link to="/works">Works</Link>
+			</LinkStyle>
+			<LinkStyle>
+				<Link to="/about">About</Link>
+			</LinkStyle>
+		</HeaderStyle>
 	)
 }
