2fa2520516e1efc3accedd76ba87f6bc91794650
kyuuri11013 <kyuuri11013@gmail.com>
2022-02-15 17:04:45 +0900

    change: top page styles

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,3 +1,14 @@
+import styled from 'styled-components'
+
+const Title = styled.div`
+	font-size: 4rem;
+	font-family: 'Italianno', cursive;
+	color: white;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+`
+
 export default function Top() {
-	return <></>
+	return <Title>fill 's portfolio</Title>
 }
